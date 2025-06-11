2d0290973c417b4723a4a606eaf76251dc87b276
Solcito <sol.medina@duocuc.cl>
2025-06-10 20:13:20 -0400

    Creando_Tablas

--- msvc-empleados/src/main/java/com/smedina/msvcempleados/model/empleado.java
+++ msvc-empleados/src/main/java/com/smedina/msvcempleados/model/empleado.java
@@ -1,8 +1,37 @@
 package com.smedina.msvcempleados.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "empleado")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class empleado {
 
+    @Id
+    @Column(name = "id_empleado", nullable = false)
     private Long idEmpleado;
-    private int runEmpleado;
+
+    private String runEmpleado;
+
+    private String nombresEmpleado;
+
+    private String apellidosEmpleado;
+
+    private Date fechaNacEmpleado;
+
+    private String direccionEmpleado;
     
 }
